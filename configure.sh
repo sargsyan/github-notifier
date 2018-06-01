7912b7ce375d9ddfeac82ce9c8ab6fc12991c856
Sergey Sargsyan <serg.sargsyan@gmail.com>
2018-06-01 14:18:11 +0200

    add configure script

---
+++ configure.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+
+. macos/keychain_accessor.sh
+
+readonly APP_NAME=$(basename $0)
+readonly SERVICE_NAME='github_notif'
+readonly CONFIG_FILE=".${SERVICE_NAME}_conf"
+
+readonly STATUS_ACTIVE='active'
+readonly STATUS_INACTIVE='inactive'
+
+function usage() {
+cat <<- EOF
+usage:
+  $APP_NAME list
+  $APP_NAME add <github instance url> [<access token>]
+  $APP_NAME rm <config name>
+  $APP_NAME activate <config name>
+  $APP_NAME deactivate <config name>
+  $APP_NAME token update <config name> [<new token>]
+
+Used to manage github instances. Normally, the most popular github instance is github.com.
+The other instances are github enterprise instances.
+
+Examples:
+   List all configurations:
+   $APP_NAME list
+
+   Add a new configuration:
+   $APP_NAME add github.mycompany.com
+   Github access token for config:
+   $APP_NAME add github.mycompany.com asasfsa23fq3dsf
+
+   Remove a configuration:
+   $APP_NAME rm my_company
+
+   Reset token
+   $APP_NAME token update my_company new_token
+EOF
+}
+
+function config_exists() {
+  local config_name=$1
+  [ "$(get_config $config_name)" != '' ]
+}
+
+function get_config() {
+  assert_is_set "Configuration name" $1
+  local config_name=$1
+  echo $(grep -w $config_name $CONFIG_FILE)
+}
+
+function add_config() {
+  local config_name=$1
+  local token=$2
+  assert_is_set "Configuration name" $config_name
+  if config_exists $config_name; then
+    show_error "Connfiguration $config_name already exists"
+    return
+  fi
+  echo $config_name $STATUS_ACTIVE >> $CONFIG_FILE
+  create_macos_config $SERVICE_NAME $config_name ${USER} $token
+}
+
+function remove_config() {
+  assert_is_set "Configuration name" $1
+  local config_name=$1
+  local line_number=$(grep -n "$config_name " $CONFIG_FILE| cut -f1 -d:)
+  if [ -n "$line_number" ]; then
+    sed -i -e "${line_number}d" $CONFIG_FILE
+    remove_macos_config $config_name $SERVICE_NAME ${USER}
+    assert_successful $? "Failed to remove $config_name"
+  fi
+}
+
+function activate_config() {
+  local config_name=$1
+  assert_is_set "config name" $config_name
+  if config_exists $config_name; then
+    update_config $config_name $STATUS_ACTIVE
+  else
+    show_error "Connfiguration $config_name does not exist"
+  fi
+}
+
+function deactivate_config() {
+  local config_name=$1
+  assert_is_set "config name" $config_name
+  if config_exists $config_name; then
+    update_config $config_name $STATUS_INACTIVE
+  else
+    show_error "Connfiguration $config_name does not exist"
+  fi
+}
+
+function update_config() {
+  local config_name=$1
+  local activity_status=$2
+  local line_number=$(grep -wn "$config_name " $CONFIG_FILE | cut -d: -f1)
+  sed -i -e "${line_number}s/.*/$config_name $activity_status/" $CONFIG_FILE
+}
+
+function update_token() {
+  local config_name=$1
+  local token=$2
+  assert_is_set "config name" $config_name
+  if config_exists $config_name; then
+    create_or_update_macos_config $SERVICE_NAME $config_name ${USER} $token
+  else
+    show_error "Connfiguration $config_name does not exist"
+  fi
+}
+
+function get_token() {
+  local config_name=$1
+  assert_is_set "config name" $config_name
+  if config_exists $config_name; then
+    get_macos_config $SERVICE_NAME $config_name
+  else
+    show_error "Connfiguration $config_name does not exist"
+  fi
+}
+
+function list_configs() {
+  cat $CONFIG_FILE
+}
+
+function route_command() {
+  ensure_config_file_is_created
+  local cmd=$1
+  case $cmd in
+    "list")
+      list_configs
+    ;;
+    "add")
+      local config_name=$2
+      local access_token=$3
+      add_config $config_name $access_token
+    ;;
+    "rm")
+      local config_name=$2
+      remove_config $config_name
+    ;;
+    "activate")
+      local config_name=$2
+      activate_config $config_name
+    ;;
+    "deactivate")
+      local config_name=$2
+      deactivate_config $config_name
+    ;;
+    "token")
+      local token_cmd=$2
+      local config_name=$3
+      local access_token=$4
+      route_token_command $token_cmd $config_name $access_token
+    ;;
+    *)
+      if [[ $cmd ]]; then
+        echo "unknown command $cmd"
+      fi
+      usage
+    ;;
+    esac
+}
+
+function route_token_command() {
+  local cmd=$1
+  local config_name=$2
+  local access_token=$3
+  case $cmd in
+    "update")
+      update_token $config_name $access_token
+    ;;
+    "")
+      usage
+    ;;
+  esac
+}
+
+function ensure_config_file_is_created() {
+  if [ ! -f $CONFIG_FILE ]; then
+    touch $CONFIG_FILE
+  fi
+}
+
+function assert_is_set() {
+  local arg_description=$1
+  local arg=$2
+  if [[ ! $arg ]]; then
+    show_error "$arg_description is not provided" && exit
+  fi
+}
+
+function assert_successful() {
+  local command_return_value=$1
+  local error_message=$2
+
+  if [ $command_return_value -ne 0 ]; then
+    echo $error_message;
+  fi
+}
+
+function show_error() {
+  local message=$1
+  [[ $message ]] && echo $message
+}
+
+route_command $1 $2 $3 $4
---
+++ test/configure_test.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+APP=./configure.sh
+
+SERVICE_NAME='github_notif'
+
+oneTimeSetUp() {
+  rm .github_notif_conf
+  source $APP > /dev/null
+}
+
+test_list_on_empty_configs() {
+  assertEquals '' "$($APP list)"
+}
+
+test_list_on_one_item() {
+  $APP add config_name token
+  assertEquals 'config_name active' "$($APP list)"
+  $APP rm config_name
+}
+
+test_list_on_two_configs() {
+  $APP add config_name token
+  $APP add config_name2 token
+  assertEquals 2 $($APP list | wc -l )
+  assertEquals 'config_name active' "$($APP list | head -n 1 )"
+  assertEquals 'config_name2 active' "$($APP list | tail -n 1 )"
+  $APP rm config_name
+  $APP rm config_name2
+}
+
+test_remove_missing() {
+  local error=$($APP rm missing)
+  assertEquals '' "$error"
+}
+
+test_add_and_get_and_remove() {
+  $APP add config_name token
+  local actual_token=$(get_token config_name)
+  assertEquals 'token' "$actual_token"
+  local error=$($APP rm config_name)
+  assertEquals '' "$error"
+}
+
+test_add_with_missing_token() {
+  $APP add config_name <<< token
+  local actual_token=$(get_token config_name)
+  assertEquals 'token' "$actual_token"
+  $APP rm config_name
+}
+
+test_add_duplicate() {
+  $APP add config_name token
+  local error=$($APP add config_name token)
+  assertEquals 'Connfiguration config_name already exists' "$error"
+  local actual_token=$(get_token config_name)
+  assertEquals 'token' "$actual_token"
+  $APP rm config_name
+}
+
+test_remove_missing_config_arg() {
+  local error=$($APP rm)
+  assertEquals 'Configuration name is not provided' "$error"
+}
+
+test_remove_missing_config() {
+  local error=$($APP rm missing)
+  assertEquals '' "$error"
+}
+
+test_activate_and_deactive_config() {
+  $APP add config_name token
+  $APP activate config_name
+  assertEquals 'config_name active' "$($APP list)"
+  $APP deactivate config_name
+  assertEquals 'config_name inactive' "$($APP list)"
+  $APP activate config_name
+  assertEquals 'config_name active' "$($APP list)"
+  $APP rm config_name
+}
+
+test_activate_missing_config() {
+  local error=$($APP activate missing)
+  assertEquals 'Connfiguration missing does not exist' "$error"
+}
+
+test_deactivate_missing_config() {
+  local error=$($APP deactivate missing)
+  assertEquals 'Connfiguration missing does not exist' "$error"
+}
+
+test_token_update_on_active_token() {
+  $APP add config_name token
+  $APP token update config_name new_token
+  local actual_token=$(get_token config_name)
+  assertEquals 'new_token' "$actual_token"
+  $APP rm config_name
+}
+
+test_token_update_on_inactive_token() {
+  $APP add config_name token
+  $APP deactivate config_name
+  $APP token update config_name new_token
+  local actual_token=$(get_token config_name)
+  assertEquals 'new_token' "$actual_token"
+  $APP rm config_name
+}
+
+test_token_update_missing_config_name() {
+  local missing=missing
+  error=$($APP token update $missing new_token)
+  assertEquals "Connfiguration $missing does not exist" "$error"
+}
+
+test_token_missing_subcommand() {
+  local error=$($APP token)
+  local usage=$($APP)
+  assertEquals "$usage" "$error"
+}
