6f1466a8c39c734e48a853a03bb303e5ea622eb0
Sergey Sargsyan <serg.sargsyan@gmail.com>
2018-09-16 16:19:20 +0200

    use absolute path for terminal notifier

--- lib/notifier.sh
+++ lib/notifier.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+readonly DIR_NAME=$(dirname $BASH_SOURCE)
 readonly EMOJIFY=$([ -x "$(command -v emojify)" ] && echo emojify || echo cat)
-TERMINAL_NOTIFIER=github-notifier.app/Contents/MacOS/terminal-notifier
+TERMINAL_NOTIFIER=$DIR_NAME/github-notifier.app/Contents/MacOS/terminal-notifier
 
 function show_notification_window() {
   local title=$(echo $1 | $EMOJIFY)
