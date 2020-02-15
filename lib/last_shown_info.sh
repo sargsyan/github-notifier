bdc3446890fecec2034e7f9d2b9758b976581f85
Sergey Sargsyan <serg.sargsyan@gmail.com>
2020-02-15 21:40:39 +0100

    Log the case when lastes_shown file is not writable

--- lib/last_shown_info.sh
+++ lib/last_shown_info.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+readonly CURR_DIR_NAME=$(dirname "$BASH_SOURCE")
 readonly LATEST_SHOWN_FILE="$HOME/.latest_shown"
+
+. "$CURR_DIR_NAME/logger.sh"
 
 function get_last_shown_commit_date() {
   local config_name=$1
@@ -16,6 +19,11 @@ function save_last_shown_commit_date() {
   local config_name=$1
   local value=$2
   if [ -f "$LATEST_SHOWN_FILE" ]; then
+    if [ ! -w "$LATEST_SHOWN_FILE" ]; then
+      # TODO: consider notifying (with exponential backup) if this really happens
+      log_error "$LATEST_SHOWN_FILE is not writable for the current user"
+      return 1
+    fi
     local line_number=$(grep -wn "$config_name" "$LATEST_SHOWN_FILE" | cut -d: -f1)
     if [[ -n "$line_number" ]]; then
       sed -i .bak "${line_number}s|.*|$config_name $value|" "$LATEST_SHOWN_FILE" &&
